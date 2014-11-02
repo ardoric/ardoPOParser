fdb84d5d1a2d49e83c63aec527546d8d71caf153
ardoric <ardoric@gmail.com>
2014-11-02 20:43:29 +0000

    initial commit
    nothing much, just a little structure of what's to come.

---
+++ ardoPOParser/POEntry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ardoPOParser
+{
+    public class POEntry
+    {
+
+        public string msgid { get; set; }
+        public string msgstr { get; set; }
+
+        // TODO plurals and stuff
+        public bool Fuzzy { get; set; }
+    }
+}
